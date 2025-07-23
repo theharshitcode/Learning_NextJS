4214012649c02059f372aa7e828d0f158c26734c
theharshitcode <harshit.cse2023@gmail.com>
2025-07-23 20:12:23 +0530

    Next.js first experience files

---
+++ app/skills/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-2xl font-bold'>My Skills</h1>
+      <ul className='list-disc pl-5'>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>MongoDB</li>
+      </ul>
+    </div>
+  )
+}
+
+export default page
